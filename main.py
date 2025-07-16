acac64fec902f8916ec9ace1400b24a8ba1e6d1a
jengzang <yangzheng031209@qq.com>
2025-07-16 23:17:36 +0800

    20250716

---
+++ main.py
@@ -0,0 +1,5 @@
+from source.raw2tsv import convert_all_to_tsv
+from source.tsv2sql import write_to_sql
+
+# convert_all_to_tsv()
+write_to_sql()
---
+++ source/config.py
@@ -0,0 +1,8 @@
+APPEND_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dependency/Append_files.xlsx"
+HAN_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dependency/漢字音典字表檔案（長期更新）.xlsx"
+PHO_TABLE_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dependency/聲韻.xlsx"
+QUERY_DB_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dialects_query.db"
+DIALECTS_DB_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dialects_all.db"
+CHARACTERS_DB_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/characters.db"
+ZHENGZI_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dependency/正字.tsv"
+MULCODECHAR_PATH = "C:/Users/joengzaang/PycharmProjects/process_phonology/data/dependency/mulcodechar.dt"
